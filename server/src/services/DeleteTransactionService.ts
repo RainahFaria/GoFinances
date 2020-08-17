d71956b7c03b5af721f3056014d5970716697d25
Rainah Faria da Silva <rainahfaria2@gmail.com>
2020-08-16 21:51:14 -0300

    add backend go finance

---
+++ server/src/services/DeleteTransactionService.ts
@@ -0,0 +1,19 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import Transactionrepository from '../repositories/TransactionsRepository';
+
+class DeleteTransactionService {
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(Transactionrepository);
+
+    const transaction = await transactionRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionRepository.remove(transaction);
+  }
+}
+
+export default DeleteTransactionService;
